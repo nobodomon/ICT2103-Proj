cae88442ee9bee4a8aace29e1d4ee6c17aeb370e
nobodomon <nobodomon@gmail.com>
2022-11-04 16:41:21 +0800

    ported tennyson data

---
+++ ExemptionChecker/server/db/Ap.sql
@@ -0,0 +1,71 @@
+INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC3003A','Integrated Work Study Programme (Career Skills)','YEAR 1 ',' 1    
+','- 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1002 ','Programming Fundamentals','YEAR 1 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1003 ','Mathematics 1','YEAR 1 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1101 ','Introduction to Computer Systems','YEAR 1 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('UCS1001 ','Critical Thinking and Communicating','YEAR 1 ',' 1    
+','4 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('UDC1001 ','Digital Competency Essentials','YEAR 1 ',' 1    
+','2 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('UDE1001 ','Introduction to Design Innovation','YEAR 1 ',' 1    
+','2 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC3003A','Integrated Work Study Programme (Career Skills)','YEAR 1 ',' 2    
+','- 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1004 ','Mathematics 2','YEAR 1 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1005 ','Web Systems and Technologies','YEAR 1 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1006 ','Computer Networks','YEAR 1 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1008 ','Data Structures and Algorithms','YEAR 1 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1009 ','Object Oriented Programming','YEAR 1 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC1001 ','Introduction to Fintech','YEAR 1 ',' 3    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC3003A','Integrated Work Study Programme (Career Skills)','YEAR 1 ',' 3    
+','- 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ACC1024 ','Foundations of Finance','YEAR 2 ',' 1   
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC3003A','Integrated Work Study Programme (Career Skills)','YEAR 2 ',' 1   
+','- 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF2001 ','Introduction to Software Engineering','YEAR 2 ',' 1   
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF2003 ','Database Systems','YEAR 2 ',' 1   
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF2006 ','Cloud Computing and Big Data','YEAR 2 ',' 1   
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('UDE2001 ','Interdisciplinary Design Innovation','YEAR 2 ',' 1   
+','4 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('AAI1001 ','Data Engineering and Visualization','YEAR 2 ',' 2   
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ACC2026 ','Investment Risk Management','YEAR 2 ',' 2   
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC2001 ','Software Requirements Engineering and Design','YEAR 2 ',' 2   
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC2002 ','Blockchain and Cryptocurrency','YEAR 2 ',' 2   
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF2008 ','Machine Learning','YEAR 2 ',' 2   
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC2003 ','Fintech projects','YEAR 2 ',' 3   
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC3001 ','Business Valuation and Analysis','YEAR 3 ',' 1   
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC3002 ','Fintech: Advanced Topics','YEAR 3 ',' 1   
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('CEG2013 ','Project Management and Professional Ethics','YEAR 3 ',' 1   
+','3 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF2005 ','Cyber Security Fundamentals','YEAR 3 ',' 1   
+','  
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC3003B','Integrated Work Study Programme (Work Attachment)','YEAR 3 ',' 2   
+','0 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC3004 ','Capstone Project','YEAR 3 ',' 2   
+','5 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC3003B','Integrated Work Study Programme (Work Attachment)','YEAR 3 ',' 3   
+','0 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('BAC3004 ','Capstone Project','YEAR 3 ',' 3   
+','5 
+');
---
+++ ExemptionChecker/server/db/Is.sql
@@ -0,0 +1,91 @@
+INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT1011 ','Computer Organization & Architecture','YEAR 1 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1001 ','Introduction to Computing','YEAR 1 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1002 ','Programming Fundamentals','YEAR 1 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1003 ','Mathematics 1','YEAR 1 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('UDC1001 ','Digital Competency Essentials','YEAR 1 ',' 1    
+','2 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT4012A','Integrated Work Study Programme (Career Skills)','YEAR 1 ',' 1    
+','- 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT1012 ','Operating Systems','YEAR 1 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1004 ','Mathematics 2','YEAR 1 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT1013 ','Computer Networks','YEAR 1 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1005 ','Web Systems & Technologies','YEAR 1 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1009 ','Object -Oriented Programming','YEAR 1 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT4012A','Integrated Work Study Programme (Career Skills)','YEAR 1 ',' 2    
+','- 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1007 ','Ethics and Professional Conducts','YEAR 1 ',' 3    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF1008 ','Data Structures and Algorithms','YEAR 1 ',' 3    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('UCS1001 ','Critical Thinking and Communicating','YEAR 1 ',' 3    
+','4 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('UDE1001 ','Introduction to Design Innovation','YEAR 1 ',' 3    
+','2 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT4012A','Integrated Work Study Programme (Career Skills)','YEAR 1 ',' 3    
+','- 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF2001 ','Introduction to Software Engineering','YEAR 2 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT2217 ','Network Security','YEAR 2 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT2212 ','Ethical Hacking','YEAR 2 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF2004 ','Embedded Systems Programming','YEAR 2 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('UDE2001 ','Interdisciplinary Design Innovation','YEAR 2 ',' 1    
+','4 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT4012A','Integrated Work Study Programme (Career Skills)','YEAR 2 ',' 1    
+','- 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT2213 ','Applied Cryptography','YEAR 2 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT2214 ','Web Security','YEAR 2 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT2215 ','Mobile and Embedded Security','YEAR 2 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('INF2008 ','Machine Learning','YEAR 2 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('UCM3001 ','Change Management','YEAR 2 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT2114 ','Integrative Team Project','YEAR 2 ',' 3    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT2216 ','Secure Software Development','YEAR 2 ',' 3    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('USI2001 ','Social Innovation Project','YEAR 2 ',' 3    
+','3 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT3212 ','Operations Security and Incident Management','YEAR 3 ',' 1    
+','  
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT3213 ','Malware Analysis and Defence','YEAR 3 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT3215 ','Digital Forensics','YEAR 3 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT3214 ','Security Analytics','YEAR 3 ',' 1    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT3217 ','Specialised R&D Project','YEAR 3 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT3219 ','Industry Certification Module','YEAR 3 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT3218 ','Security Governance, Risk Management and Compliance','YEAR 3 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT3216 ','Special Topics in Security','YEAR 3 ',' 2    
+','6 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT4011 ','Capstone Project','YEAR 3 ',' 3   
+','5 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT4012B','Integrated Work Study Programme (Work Attachment)','YEAR 3 ',' 3   
+','0 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT4011 ','Capstone Project','YEAR 4 ',' 1   
+','5 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT4012B','Integrated Work Study Programme (Work Attachment)','YEAR 4 ',' 1   
+','0 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT4011 ','Capstone Project','YEAR 4 ',' 2   
+','5 
+');INSERT INTO UniversityModules (moduleCode,moduleName,yearOffered,period,credits) VALUES ('ICT4012B','Integrated Work Study Programme (Work Attachment)','YEAR 4 ',' 2   
+','0 
+');
