8cf3726cd2183a3b5339570152ae885deadac44e
Janice Lim <janicelimlq@gmail.com>
2022-11-06 02:52:03 +0800

    added in the sql scripts that were ran

---
+++ ExemptionChecker/server/db/UniversityCourses_Insert_Script.sql
@@ -0,0 +1,5 @@
+INSERT INTO UniversityCourses (courseCode, courseName, university)
+VALUES
+('BENG SE', 'Information & Communications Technology majoring in Software Engineering', 5),
+('BENG IS', 'Information & Communications Technology majoring in Information Security', 5),
+('BSC AP', 'Applied Computing', 5);
